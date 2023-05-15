2cf0e352b2313f393ee14c6c05d405611961e1b3
Sankit2512 <119419693+Sankit2512@users.noreply.github.com>
2023-05-15 08:27:28 +0530

    Update Error.py
    Credits : Sankit

--- Error.py
+++ Error.py
@@ -1,1 +1,16 @@
 
+Error_Code = {
+    100 : "Overflow",
+    101 : "Typos in instruction name or register name",
+    102 : "Use of undefined variables",
+    103 : "Use of undefined labels",
+    104 : "Illegal use of FLAGS registor",
+    105 : "Illegal immediate value",
+    106 : "Misuse of labels as variables or visa versa",
+    107 : "Variables not declared at the begining",
+    108 : "Missing hlt instruction",
+    109 : "hlt not being used as last instruction",
+    110 : "Syntax Error",
+    111 : "More than one hlt",
+    404 : "No Error"
+}
